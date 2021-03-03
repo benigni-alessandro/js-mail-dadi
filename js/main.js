9c547fb1e45182e64b6777a30428a120304ee79c
alessandro <alebeni02@gmail.com>
2021-03-03 10:38:08 +0100

    correzione parentesi

--- js/main.js
+++ js/main.js
@@ -2,17 +2,18 @@ var emailutente = prompt("Inserisci la tua email");
 // document.getElementById('emailutente').innerHTML = "Nome: " + nome;
 console.log(emailutente);
 
-var listaemail = ("pincopallo@gmail.com", "gianluca@gmail.com", "alessandro@gmail.com", "marco@gmail.com", "paolo@gmail.com" );
+var listaemail = ['pincopallo@gmail.com', 'gianluca@gmail.com', 'alessandro@gmail.com', 'marco@gmail.com', 'paolo@gmail.com'];
+
 var soldatino = false;
 
 for (var i = 0; i < listaemail.length; i++) {
-  if (listaemail[i] == emailutente) {
+  if (listaemail[i] === emailutente) {
     soldatino = true;
   }
 }
 
 if (soldatino) {
-  console.log("Accesso negato");
-} else {
   console.log("Accesso consentito");
+} else {
+  console.log("Accesso negato");
 }
